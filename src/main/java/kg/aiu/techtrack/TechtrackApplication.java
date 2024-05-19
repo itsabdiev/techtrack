8986c0039346a7afb8bfe2d83ee102d7578a840c
itsabdiev <atai.abdiev.dev@gmail.com>
2024-05-19 23:26:03 +0600

    initialized base project with 5 dependencies

---
+++ src/main/java/kg/aiu/techtrack/TechtrackApplication.java
@@ -0,0 +1,13 @@
+package kg.aiu.techtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtrackApplication.class, args);
+	}
+
+}
